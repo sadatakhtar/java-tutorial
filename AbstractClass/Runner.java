a629a7e1a974ab533e0b7edad1f837c8dfefcfc8
sadat <sadat0akhtar@gmail.com>
2024-01-22 17:01:05 +0000

    updated file

--- AbstractClass/Runner.java
+++ AbstractClass/Runner.java
@@ -2,8 +2,8 @@ package AbstractClass;
 
 public class Runner {
     public static void main(String[] args) {
-        // Dog dog = new Dog();
-        // dog.bark();
+        Dog dog = new Dog();
+        dog.bark();
         Recipe recipe = new Recipe();
         RecipeWithMicrowave rec2 = new RecipeWithMicrowave();
 
